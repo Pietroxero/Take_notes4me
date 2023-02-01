7f8010ffde1340cffc843a167123fa16da378659
Pietroxero <115331791+Pietroxero@users.noreply.github.com>
2023-01-31 20:04:04 -0800

    added in dependencies within server.js file

---
+++ server.js
@@ -0,0 +1,23 @@
+//this will be the dependencies needed to invoke the scrip
+
+const express = require('express');
+const htmlRoute = require('./routes/htmlroutes');
+
+
+//this will show the configuration for express server
+
+const app = express();
+
+//sets the app to process data parsing
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('./Develop/public'));
+
+//this will be the router
+//this points our server to the 'route' file.
+
+app/use('./', htmlRoute)
+
+//sets an initial port, to be used in the listener
+const PORT = process.env.PORT || 3001;
