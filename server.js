8beb85facaa49b2b815e9a524e11b159ba99226a
Pietroxero <115331791+Pietroxero@users.noreply.github.com>
2023-01-31 20:17:56 -0800

    added in new const to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const htmlRoute = require('./routes/htmlroutes');
+const apiRoute = require('./routes/apiroutes');
 
 
 //this will show the configuration for express server
@@ -17,7 +18,14 @@ app.use(express.static('./Develop/public'));
 //this will be the router
 //this points our server to the 'route' file.
 
-app/use('./', htmlRoute)
+app.use('./', htmlRoute)
+app.use('/api', apiRoute);
 
 //sets an initial port, to be used in the listener
 const PORT = process.env.PORT || 3001;
+
+//listener
+
+app.listen(PORT, () => 
+console.log(`app is listening for PORT ${PORT}`)
+);
