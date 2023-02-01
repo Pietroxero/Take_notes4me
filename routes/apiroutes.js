75542a916f85df783bcd31b3d60f4717df4602ff
Pietroxero <115331791+Pietroxero@users.noreply.github.com>
2023-02-01 11:23:26 -0800

    added in function for adding and deleting notes to api routes js file

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -5,11 +5,33 @@ const router = required('express').Router();
 const uid = require('uuid');
 const fs = require('fs');
 const until = require('util');
-const writeFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 const readFileAsync = util.promisify(fs.readFile);
 const writeFile = data => {return writeFileAsync('../Develop/db/db.json', JSON.stringify(data))};
 const readFile = () => {return readFileAsync('./Develop/db/db.json', 'utf8')};
 
 //routes
+//return notes
+router.get('/notes', (req, res) => {
+    getNotes().then((notes) => {
+        return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+//post request for adding/remove notes
+router.delete('/notes/:id', (req, res) => {
+    deleteNote(req.params.id)
+    .then(() => res.json({status: true}))
+    .catch((err) =>res.status(500).json(err))
+});
+
+router.post('/notes', (req, res) => {
+    getNotes(req.body)
+.then((note) => res.json(note))
+.catch((err) => res.status(500).json(err))
+});
+
+//here we return a list of notes
 
 
